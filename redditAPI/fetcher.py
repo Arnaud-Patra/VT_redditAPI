16bae7b499baa929998c597193dfb8b10c87b35a
Arnaud Patra <arnaudpatra@hotmail.fr>
2019-08-14 16:58:10 +0200

    before day off

--- redditAPI/fetcher.py
+++ redditAPI/fetcher.py
@@ -1,5 +1,5 @@
-# import re
-# import requests
+import re
+import requests
 import praw
 
 
@@ -10,8 +10,7 @@ import praw
 #                      password='sxks*63k',
 #                      user_agent='prawFetcher')
 
-"""With init"""
-reddit = praw.Reddit('bot1', user_agent='bot1 user agent')
+
 
 # subreddit = reddit.subreddit('Dankmemes')
 
@@ -28,13 +27,32 @@ reddit = praw.Reddit('bot1', user_agent='bot1 user agent')
 #                                                                                submission.visited))
 
 
-EarthPorn = reddit.subreddit('EarthPorn')
-
-top_all_EarthPorn = EarthPorn.top('day', limit=10)
-for submission in top_all_EarthPorn:
-    if not submission.stickied:
-        print(submission.url)
+# top_all_EarthPorn = EarthPorn.top('day', limit=1)
+#     for submission in top_all_EarthPorn:
+#         if not submission.stickied:
+#             print(submission.url)
 
 
+def subfetcher():
+
+    """With init"""
+    reddit = praw.Reddit('bot1', user_agent='bot1 user agent')
+
+    """Open subreddits"""
+    with open('subreddits.txt') as subs_txt:
+        for sub in subs_txt:
+            subreddit = reddit.subreddit(sub)
+            top_post = subreddit.top('day', limit=1)
+            if not top_post.stickied:
+                print(top_post.url)
+            else:
+                print("> top post is stickied")
+
+def get_top(sub):
+    # subreddit = reddit.subreddit(sub)
+    pass
 
 
+if __name__ == "__main__":
+    # execute only if run as a script
+    subfetcher()
